6d6235e2fe26ac684408e57b986beb8516354217
Yaiza Vallejo Caro <yaizavallejo16@gmail.com>
2022-06-19 11:21:34 +0200

    remove multer

--- index.js
+++ index.js
@@ -11,11 +11,12 @@ let app = express();
 connectDb();
 
 // MIDDLEWARES
-app.use(cors()); // Use this after the variable declaration
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+// ROUTES
 app.use("/api/products", productRouter);
 
 const PORT = process.env.PORT || 5000;
