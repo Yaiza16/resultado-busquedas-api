707df3f3ad3adaa48c5801ffeca9a0af44b816ba
Yaiza Vallejo Caro <yaizavallejo16@gmail.com>
2022-06-19 10:04:29 +0200

    add toaster

--- client/src/components/ProductForm/ProductForm.jsx
+++ client/src/components/ProductForm/ProductForm.jsx
@@ -3,71 +3,76 @@ import React from 'react'
 import { ErrorMessage, Field, Form, Formik } from 'formik'
 import * as Yup from 'yup'
 import axios from 'axios'
+import toast, { Toaster } from 'react-hot-toast'
 import convertFileToBase64 from '../../utils/convertToBase64'
-// import convertFileToBase64 from '../../utils/convertToBase64'
-// import axios from 'axios'
 
 const ProductFormSchema = Yup.object().shape({
   displayName: Yup.string()
-    .max(50, 'Menos de 50 carácteres')
-    .required('Requerido'),
-  price: Yup.number()
-    .min(1, 'El precio tiene que ser mayor a 1')
-    .required('Requerido'),
+    .max(50, 'Product name must have less or equal then 40 characters')
+    .required('Required'),
+  price: Yup.number().min(1, 'Price must be above 1').required('Required'),
 })
 
 function ProductForm() {
+  const notify = () => toast.success('Product uploaded successfully')
+
   return (
-    <Formik
-      initialValues={{
-        displayName: '',
-        price: 0,
-        image: undefined,
-      }}
-      validationSchema={ProductFormSchema}
-      onSubmit={async (values) => {
-        await axios.post('http://localhost:5000/api/products', values)
-      }}
-    >
-      {({ setFieldValue, errors, touched }) => (
-        <Form className="form-products-container">
-          <div className="form-field-container">
-            <label htmlFor="displayName">Name of product</label>
-            <Field type="text" name="displayName" />
-            {errors.displayName && touched.displayName ? (
-              <span>{errors.displayName}</span>
-            ) : null}
-          </div>
-          <div className="form-field-container">
-            <label htmlFor="price">Price</label>
-            <Field type="number" name="price" />
-            <ErrorMessage
-              name="price"
-              className="error-msg-input"
-              component="span"
-            />
-          </div>
+    <>
+      <Toaster position="top-right" />
+      <Formik
+        initialValues={{
+          displayName: '',
+          price: 0,
+          image: undefined,
+        }}
+        validationSchema={ProductFormSchema}
+        onSubmit={async (values) => {
+          await axios.post('http://localhost:5000/api/products', values)
+          notify()
+        }}
+      >
+        {({ setFieldValue }) => (
+          <Form className="form-products-container">
+            <div className="form-field-container">
+              <label htmlFor="displayName">Name of product</label>
+              <Field type="text" name="displayName" />
+              <ErrorMessage
+                name="displayName"
+                className="error-msg-input"
+                component="span"
+              />
+            </div>
+            <div className="form-field-container">
+              <label htmlFor="price">Price</label>
+              <Field type="number" name="price" />
+              <ErrorMessage
+                name="price"
+                className="error-msg-input"
+                component="span"
+              />
+            </div>
 
-          <div className="form-field-container">
-            <label htmlFor="image">Image</label>
-            <Field
-              type="file"
-              name="image"
-              value={undefined}
-              onChange={async (e) =>
-                setFieldValue(
-                  'image',
-                  await convertFileToBase64(e.target.files[0])
-                )
-              }
-            />
-            <ErrorMessage name="image" />
-          </div>
+            <div className="form-field-container">
+              <label htmlFor="image">Image</label>
+              <Field
+                type="file"
+                name="image"
+                value={undefined}
+                onChange={async (e) =>
+                  setFieldValue(
+                    'image',
+                    await convertFileToBase64(e.target.files[0])
+                  )
+                }
+              />
+              <ErrorMessage name="image" />
+            </div>
 
-          <button type="submit">Upload</button>
-        </Form>
-      )}
-    </Formik>
+            <button type="submit">Upload</button>
+          </Form>
+        )}
+      </Formik>
+    </>
   )
 }
 
