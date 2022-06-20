3a07cfb96f192626fd4a6877e237e027efff40fd
Yaiza Vallejo Caro <yaizavallejo16@gmail.com>
2022-06-20 17:01:05 +0200

    add animation

--- client/services/productsApi.js
+++ client/services/productsApi.js
@@ -1,7 +1,6 @@
+/* eslint-disable import/prefer-default-export */
 import axios from 'axios'
 
 export const baseUrl = axios.create({
   baseURL: 'localhost:5000/api/products',
 })
-
-export const baseRandom = ''
